b960e5d8dd79432037a601ce38031c09339add85
Arik92 <arik92@gmail.com>
2017-06-14 15:10:23 +0300

    added public folder and npm(might add ajax too)

---
+++ public/controller.js
@@ -0,0 +1,15 @@
+app.controller('emCtrl', function($scope, emService){
+  $scope.getAllPlayLists = function() {
+    emService.getAllPlayLists().then(function(err, data){
+      if (err) {
+        console.error("controller error fetching playlists", err);
+      } else {
+        $scope.allPlayLists = data;
+        console.log("controller data", data);
+    }
+    }//function
+  )//callback
+}//getallplaylists
+$scope.getAllPlayLists();
+
+});//controller
